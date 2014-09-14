db7cd3a78d1d437b1ad1e0c05f22c4089bb2baaf
Lewis Jones <ljones140@gmail.com>
2014-09-14 20:29:00 +0100

    using mt_rand to get better results

--- favouritewords.php
+++ favouritewords.php
@@ -5,22 +5,30 @@
         header("Refresh: $sec; url=$page");
 
 	require('connections.php'); 
+	
+	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        $query = "SELECT count(*) as fontcount FROM font";
+        $result = mysqli_query($dbc, $query);
+        while ($row = mysqli_fetch_assoc($result)){
+                $fontcount = $row['fontcount'];
 
+        }
+        
+        mysqli_close($dbc);
+
+        $randfont = mt_rand(1,$fontcount);
+
+	
 
         $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        $query ="SELECT font FROM font ".
-                "WHERE fontid >= ( ".
-                "SELECT FLOOR( MAX( fontid ) * RAND( ) ) ".
-                "FROM font ) ".
-                "ORDER BY fontid ".
-                "LIMIT 1";
+        $query ="SELECT font FROM font WHERE fontid = $randfont ";
 
         $result = mysqli_query($dbc, $query);
         while ($row = mysqli_fetch_assoc($result)) {
                 $font = $row['font'];
-                mysqli_close($dbc);
         }
 
+        mysqli_close($dbc);
 
 
 ?>
@@ -36,15 +44,15 @@
 
 	<style type="text/css">
 	.word {
-		font-size: 50pt;
+		font-size: 100pt;
 		color: green;
 		<?php echo $font; ?>
 		margin: auto;
-		position: absolute;
-  		top: 35%; 
-  		left: 50%; 
-  		bottom: 65%; 
-  		right: 50%;
+		position: absolute; 
+  		top: 20%; 
+  		left: 40%; 
+  		bottom: 80%; 
+  		right: 60%;
 
 
 
@@ -69,25 +77,28 @@
 	//get the count of words to do random calc
 
 	
-//	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-//	$query = "SELECT count(*) FROM word";
+	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$query = "SELECT count(*) as wordcount FROM word";
+	$result = mysqli_query($dbc, $query);
+	while ($row = mysqli_fetch_assoc($result)){
+		$wordcount = $row['wordcount'];
+		
+	}
+	
+	mysqli_close($dbc);
 
-//	$result = mysqli_query($dbc, $query);
-//	while ($row = mysqli_query($dbc, $query)
+	$randword = mt_rand(1,$wordcount);
 
 	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	$query ="SELECT word FROM word ".
-		"WHERE wordid >= ( ". 
-		"SELECT FLOOR( MAX( wordid ) * RAND( ) ) ". 
-		"FROM word ) ".
-		"ORDER BY wordid ".
-		"LIMIT 1";	
+		"WHERE wordid = $randword"; 
 
 	$result = mysqli_query($dbc, $query);
   	while ($row = mysqli_fetch_assoc($result)) {
   		$word = $row['word'];
-		mysqli_close($dbc);
 	}
+	
+	mysqli_close($dbc);
 	
 	echo '<p>' . $word . '</p>';
 
